091983cfd211fc6fbcffc6af764c742c756e0afb
Thomas Robitaille <thomas.robitaille@gmail.com>
2017-07-01 12:33:52 +0100

    Refactor API access for GitHub

--- baldrick/github/github_api.py
+++ baldrick/github/github_api.py
@@ -1,71 +1,142 @@
+import base64
 import requests
 
 from changebot.github_auth import github_request_headers
 
-__all__ = ['submit_review', 'set_status', 'fill_pull_request_from_issue']
+__all__ = ['RepoHandler', 'PullRequestHandler']
+
+HOST = "https://api.github.com"
 
 
-def submit_review(pull_request_payload, decision, body):
-    """
-    Submit a review comment to a pull request on GitHub.
+class RepoHandler(object):
 
-    Parameters
-    ----------
-    pull_request_payload : dict
-        The payload sent from GitHub via the webhook interface
-    decision : { 'approve' | 'request_changes' | 'comment' }
-        The decision as to whether to aprove or reject the changes so far.
-    body : str
-        The body of the review comment
-    """
+    def __init__(self, repo, branch, installation):
+        self.repo = repo
+        self.branch = branch
+        self.installation = installation
+        self._cache = {}
 
-    url_review = pull_request_payload['pull_request']['review_comments_url'].replace('comments', 'reviews')
+    def invalidate_cache(self):
+        self._cache.clear()
 
-    data = {}
-    data['commit_id'] = pull_request_payload['pull_request']['head']['sha']
-    data['body'] = body
-    data['event'] = decision.upper()
+    @property
+    def _headers(self):
+        return github_request_headers(self.installation)
 
-    headers = github_request_headers(pull_request_payload['installation']['id'])
+    @property
+    def _url_contents(self):
+        return f'{HOST}/repos/{self.repo}/contents/'
 
-    requests.post(url_review, json=data, headers=headers)
-
-
-def set_status(pull_request_payload, state, description, context):
-    """
-    Set status message in a pull request on GitHub.
-
-    Parameters
-    ----------
-    pull_request_payload : dict
-        The payload sent from GitHub via the webhook interface.
-    state : { 'pending' | 'error' | 'pass' }
-        The state to set for the pull request.
-    description : str
-        The message that appears in the status line.
-    context : str
-         A string used to identify the status line.
-    """
-
-    url_status = pull_request_payload['pull_request']['statuses_url']
-
-    data = {}
-    data['state'] = state
-    data['description'] = description
-    data['context'] = context
-
-    headers = github_request_headers(pull_request_payload['installation']['id'])
-
-    requests.post(url_status, json=data, headers=headers)
+    def get_file_contents(self, path_to_file):
+        url_file = self._url_contents + path_to_file
+        data = {'ref': self.branch}
+        response = requests.get(url_file, params=data, headers=self._headers)
+        assert response.ok, response.content
+        contents_base64 = response.json()['content']
+        return base64.b64decode(contents_base64).decode()
 
 
-def fill_pull_request_from_issue(pull_request_payload):
+class PullRequestHandler(object):
 
-    url_pull_request = pull_request_payload['issue']['pull_request']['url']
+    def __init__(self, repo, number, installation):
+        self.repo = repo
+        self.number = number
+        self.installation = installation
+        self._cache = {}
 
-    headers = github_request_headers(pull_request_payload['installation']['id'])
+    def invalidate_cache(self):
+        self._cache.clear()
 
-    response = requests.get(url_pull_request, headers=headers)
+    @property
+    def _headers(self):
+        return github_request_headers(self.installation)
 
-    pull_request_payload['pull_request'] = response.json()
-    pull_request_payload['number'] = pull_request_payload['pull_request']['number']
+    @property
+    def _url_pull_request(self):
+        return f'{HOST}/repos/{self.repo}/pulls/{self.number}'
+
+    @property
+    def _url_review_comment(self):
+        return f'{self._url_pull_request}/reviews'
+
+    @property
+    def _url_head_status(self):
+        return f'{HOST}/repos/{self.repo}/statuses/{self.head_sha}'
+
+    @property
+    def _url_labels(self):
+        return f'{HOST}/repos/{self.repo}/issues/{self.number}/labels'
+
+    @property
+    def json(self):
+        if 'json' not in self._cache:
+            response = requests.get(self._url_pull_request, headers=self._headers)
+            assert response.ok, response.content
+            self._cache['json'] = response.json()
+        return self._cache['json']
+
+    @property
+    def head_sha(self):
+        return self.json['head']['sha']
+
+    @property
+    def head_branch(self):
+        return self.json['head']['ref']
+
+    @property
+    def milestone(self):
+        milestone = self.json['milestone']
+        if milestone is None:
+            return ''
+        else:
+            return milestone['title']
+
+    @property
+    def labels(self):
+        response = requests.get(self._url_labels, headers=self._headers)
+        assert response.ok, response.content
+        return [label['name'] for label in response.json()]
+
+    def submit_review(self, decision, body):
+        """
+        Submit a review comment to the pull request
+
+        Parameters
+        ----------
+        decision : { 'approve' | 'request_changes' | 'comment' }
+            The decision as to whether to aprove or reject the changes so far.
+        body : str
+            The body of the review comment
+        """
+
+        data = {}
+        data['commit_id'] = self.head_sha
+        data['body'] = body
+        data['event'] = decision.upper()
+
+        response = requests.post(self._url_review_comment, json=data, headers=self._headers)
+        assert response.ok, response.content
+
+    def set_status(self, state, description, context):
+        """
+        Set status message in a pull request on GitHub.
+
+        Parameters
+        ----------
+        pull_request_payload : dict
+            The payload sent from GitHub via the webhook interface.
+        state : { 'pending' | 'error' | 'pass' }
+            The state to set for the pull request.
+        description : str
+            The message that appears in the status line.
+        context : str
+             A string used to identify the status line.
+        """
+
+        data = {}
+        data['state'] = state
+        data['description'] = description
+        data['context'] = context
+
+        response = requests.post(self._url_head_status, json=data, headers=self._headers)
+        assert response.ok, response.content
