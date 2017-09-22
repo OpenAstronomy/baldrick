26fd50a4a0f961c87ec0299e80a1744550adb55f
Thomas Robitaille <thomas.robitaille@gmail.com>
2017-09-22 11:08:23 -0400

    Fix text wrap

--- baldrick/blueprints/stale_pull_requests.py
+++ baldrick/blueprints/stale_pull_requests.py
@@ -3,23 +3,13 @@ from changebot.github_api import PullRequestHandler, RepoHandler
 
 
 PRS_CLOSE_WARNING = """
-Hi humans :wave: - this PR hasn't had any new commits for approximately 5
-months. Of course, in this day and age we all have way too much on our plates
-(especially me!) but in the interest on making sure that we don't keep pull
-requests open if they are no longer relevant, **I plan to close this in a month if
-the pull request doesn't have any new commits by then.**
+Hi humans :wave: - this PR hasn't had any new commits for approximately 5 months. Of course, in this day and age we all have way too much on our plates (especially me!) but in the interest on making sure that we don't keep pull requests open if they are no longer relevant, **I plan to close this in a month if the pull request doesn't have any new commits by then.**
 
-If you **really** want to keep this PR open beyond because it needs more
-discussion, then you can get a maintainer to add the **keep-open** label,
-but please only use this in rare cases. A better solution if you don't agree to
-merge this now is to close this and open a new issue to remind ourselves this
-should be done (for example if this PR is the wrong approach).
+If you **really** want to keep this PR open beyond because it needs more discussion, then you can get a maintainer to add the **keep-open** label, but please only use this in rare cases. A better solution if you don't agree to merge this now is to close this and open a new issue to remind ourselves this should be done (for example if this PR is the wrong approach).
 
-In any case, I will close this within a month unless there is a new commit
-or the **keep-open** label has been added. Thanks!
+In any case, I will close this within a month unless there is a new commit or the **keep-open** label has been added. Thanks!
 
-*If you believe I commented on this issue incorrectly, please report this at
-https://github.com/astropy/astropy-bot/issues*
+*If you believe I commented on this issue incorrectly, please report this at https://github.com/astropy/astropy-bot/issues*
 """
 
 
@@ -30,13 +20,9 @@ def is_close_warning(message):
 PRS_CLOSE_EPILOGUE = """
 :alarm_clock: Time's up! :alarm_clock:
 
-I'm going to close this pull request as per my previous message. If you think
-what is being added/fixed here is still important, please remember to open an
-issue to keep track of it. Thanks!
+I'm going to close this pull request as per my previous message. If you think what is being added/fixed here is still important, please remember to open an issue to keep track of it. Thanks!
 
-*If this is the first time I am commenting on this issue, or if you believe I
-closed this issue incorrectly, please report this at
-https://github.com/astropy/astropy-bot/issues*
+*If this is the first time I am commenting on this issue, or if you believe I closed this issue incorrectly, please report this at https://github.com/astropy/astropy-bot/issues*
 """
 
 
