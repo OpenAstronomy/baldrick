4715caa99a5f24fc4435c0c65d14b059bce1a976
Thomas Robitaille <thomas.robitaille@gmail.com>
2018-07-19 17:17:28 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(version='0.0.dev0',
+      name="baldrick",
+      description="Helpers for GitHub bots",
+      url='https://github.com/astrofrog/baldrick',
+      packages=find_packages(),
+      author='Stuart Mumford and Thomas Robitaille')
