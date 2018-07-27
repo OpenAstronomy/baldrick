9ea141166adfd2f08993408931efe952d7c71c67
Thomas Robitaille <thomas.robitaille@gmail.com>
2018-07-27 20:40:16 +0100

    Don't require milestones section to be present for milestone plugin to work - importing the milestones plugin should be enough.

--- baldrick/plugins/github_milestones.py
+++ baldrick/plugins/github_milestones.py
@@ -1,5 +1,5 @@
-from .github_pull_requests import pull_request_handler
-
+from baldrick.plugins.github_pull_requests import pull_request_handler
+from baldrick.plugins.utils import get_config_with_app_defaults
 
 MISSING_MESSAGE = 'This pull request has no milestone set.'
 PRESENT_MESSAGE = 'This pull request has a milestone set.'
@@ -11,10 +11,7 @@ def process_milestone(pr_handler, repo_handler):
     A very simple set a failing status if the milestone is not set.
     """
 
-    mc_config = repo_handler.get_config_value("milestones", None)
-
-    if mc_config is None:
-        return {}
+    mc_config = get_config_with_app_defaults(pr_handler, "milestones", {})
 
     fail_message = mc_config.get("missing_message", MISSING_MESSAGE)
     pass_message = mc_config.get("present_message", PRESENT_MESSAGE)
