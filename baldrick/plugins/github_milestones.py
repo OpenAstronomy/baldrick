970ae05dc5c3e192fd7e1bbb36e03819603fab33
Thomas Robitaille <thomas.robitaille@gmail.com>
2018-07-27 18:09:53 +0100

    Fix bug in milestones plugin

--- baldrick/plugins/github_milestones.py
+++ baldrick/plugins/github_milestones.py
@@ -14,7 +14,7 @@ def process_milestone(pr_handler, repo_handler):
     mc_config = repo_handler.get_config_value("milestones", None)
 
     if mc_config is None:
-        return [], None
+        return {}
 
     fail_message = mc_config.get("missing_message", MISSING_MESSAGE)
     pass_message = mc_config.get("present_message", PRESENT_MESSAGE)
