f14a12d2d2510ca219b4c1e8ce06191c781b74a8
Stuart Mumford <stuart@cadair.com>
2018-07-30 15:49:15 +0100

    Write tests for milestone checker, also skip if config not present

--- baldrick/plugins/github_milestones.py
+++ baldrick/plugins/github_milestones.py
@@ -1,4 +1,5 @@
 from baldrick.plugins.github_pull_requests import pull_request_handler
+from baldrick.plugins.utils import get_config_with_app_defaults
 
 MISSING_MESSAGE = 'This pull request has no milestone set.'
 PRESENT_MESSAGE = 'This pull request has a milestone set.'
@@ -10,7 +11,9 @@ def process_milestone(pr_handler, repo_handler):
     A very simple set a failing status if the milestone is not set.
     """
 
-    mc_config = pr_handler.get_config_value("milestones", {})
+    mc_config = pr_handler.get_config_value("milestones", None)
+    if mc_config is None:
+        return
 
     fail_message = mc_config.get("missing_message", MISSING_MESSAGE)
     pass_message = mc_config.get("present_message", PRESENT_MESSAGE)
---
+++ baldrick/plugins/tests/test_github_milestones.py
@@ -0,0 +1,105 @@
+from unittest.mock import patch, PropertyMock
+
+from baldrick.github.github_api import cfg_cache
+from baldrick.github.github_api import RepoHandler, PullRequestHandler
+from baldrick.plugins.github_milestones import process_milestone, MISSING_MESSAGE, PRESENT_MESSAGE
+
+
+CONFIG_TEMPLATE = """
+[ tool.testbot ]
+[ tool.testbot.milestones ]
+missing_message="{missing}"
+present_message="{present}"
+"""
+
+CONFIG_TEMPLATE_DEFAULT = """
+[ tool.testbot ]
+[ tool.testbot.milestones ]
+"""
+
+CONFIG_TEMPLATE_MISSING = """
+[ tool.testbot ]
+"""
+
+
+class TestMilestonePlugin:
+
+    def setup_method(self, method):
+
+        self.get_file_contents_mock = patch('baldrick.github.github_api.PullRequestHandler.get_file_contents')
+        self.get_base_branch_mock = patch('baldrick.github.github_api.PullRequestHandler.base_branch')
+        a = self.get_base_branch_mock.start()
+        a.return_value = "master"
+        self.milestone_mock = patch('baldrick.github.github_api.PullRequestHandler.milestone',
+                                    new_callable=PropertyMock)
+
+        self.repo_handler = RepoHandler("nota/repo", "1234")
+        self.pr_handler = PullRequestHandler("nota/repo", "1234")
+
+        self.milestone = self.milestone_mock.start()
+        self.milestone.return_value = None
+
+        self.get_file_contents = self.get_file_contents_mock.start()
+        cfg_cache.clear()
+
+    def teardown_method(self, method):
+        self.get_file_contents_mock.stop()
+        self.milestone_mock.stop()
+        self.get_base_branch_mock.stop()
+
+    def test_milestone_present(self, app):
+
+        self.get_file_contents.return_value = CONFIG_TEMPLATE.format(missing="missing milestone",
+                                                                     present="milestone present")
+        self.milestone.return_value = "0.1"
+
+        with app.app_context():
+            ret = process_milestone(self.pr_handler, self.repo_handler)
+
+        assert "milestone" in ret
+        assert len(ret) == 1
+        assert ret['milestone']['state'] == "success"
+        assert ret['milestone']['description'] == "milestone present"
+
+    def test_milestone_absent(self, app):
+
+        self.get_file_contents.return_value = CONFIG_TEMPLATE.format(missing="missing milestone",
+                                                                     present="milestone present")
+        with app.app_context():
+            ret = process_milestone(self.pr_handler, self.repo_handler)
+
+        assert "milestone" in ret
+        assert len(ret) == 1
+        assert ret['milestone']['state'] == "failure"
+        assert ret['milestone']['description'] == "missing milestone"
+
+    def test_milestone_present_default(self, app):
+
+        self.get_file_contents.return_value = CONFIG_TEMPLATE_DEFAULT
+        self.milestone.return_value = "0.1"
+
+        with app.app_context():
+            ret = process_milestone(self.pr_handler, self.repo_handler)
+
+        assert "milestone" in ret
+        assert len(ret) == 1
+        assert ret['milestone']['state'] == "success"
+        assert ret['milestone']['description'] == PRESENT_MESSAGE
+
+    def test_milestone_absent_default(self, app):
+
+        self.get_file_contents.return_value = CONFIG_TEMPLATE_DEFAULT
+        with app.app_context():
+            ret = process_milestone(self.pr_handler, self.repo_handler)
+
+        assert "milestone" in ret
+        assert len(ret) == 1
+        assert ret['milestone']['state'] == "failure"
+        assert ret['milestone']['description'] == MISSING_MESSAGE
+
+    def test_no_config(self, app):
+        self.get_file_contents.return_value = CONFIG_TEMPLATE_MISSING
+        with app.app_context():
+            ret = process_milestone(self.pr_handler, self.repo_handler)
+
+        assert ret is None
