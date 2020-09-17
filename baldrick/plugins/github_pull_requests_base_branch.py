14f65dfc7e74f89445c18553ba5fad8655fe1063
Pey Lian Lim <2090236+pllim@users.noreply.github.com>
2020-09-17 16:39:52 -0400

    Sync and update return dict

--- baldrick/plugins/github_pull_requests_base_branch.py
+++ baldrick/plugins/github_pull_requests_base_branch.py
@@ -3,8 +3,8 @@ from loguru import logger
 from .github_pull_requests import pull_request_handler
 
 
-# Only run this check once when a PR is opened.
-@pull_request_handler(actions=['opened'])
+# Only run this check once when a PR is opened or sync-ed.
+@pull_request_handler(actions=['opened', 'synchronize'])
 def check_base_branch(pr_handler, repo_handler):
     logger.trace(f'Running base branch checker for {pr_handler.repo}#{pr_handler.number}')
     cl_config = repo_handler.get_config_value("basebranch_checker", {})
@@ -17,10 +17,13 @@ def check_base_branch(pr_handler, repo_handler):
     pr_basebranch = pr_handler.base_branch
 
     if pr_basebranch == basebranch:
-        logger.info(f'PR opened correctly against {basebranch}')
-        return
+        desc = f'PR opened correctly against {basebranch}'
+        sta = 'success'
+    else:
+        desc = f'PR opened against {pr_basebranch}, not {basebranch}'
+        sta = 'failure'
 
     return {'basebranch': {
             'title': f'Change base to {basebranch}',
-            'description': f'PR opened against {pr_basebranch}, not {basebranch}',
-            'state': 'failure'}}
+            'description': desc,
+            'state': sta}}
