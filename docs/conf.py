e5fc7607f37a236c8a608f1b7f7cab24b69993ef
Stuart Mumford <stuart@cadair.com>
2020-10-06 13:17:28 +0100

    import move

--- docs/conf.py
+++ docs/conf.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
 #
 # Configuration file for the Sphinx documentation builder.
-#
-# This file does only contain a selection of the most common options. For a
-# full list see the documentation:
-# http://www.sphinx-doc.org/en/master/config
 
+from baldrick import __version__
 
 # -- Project information -----------------------------------------------------
 
@@ -14,7 +11,6 @@ author = 'Stuart Mumford, Thomas Robitaille, Pey Lian Lim, and Brigitta Sipőcz'
 copyright = '2020, ' + author
 
 # The full version, including alpha/beta/rc tags
-from baldrick import __version__
 release = __version__
 
 # -- General configuration ---------------------------------------------------
